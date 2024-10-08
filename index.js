bd63f7eec399a913d072cf7dba9a5973f450021a
Justin <justin.a.manning@accenture.com>
2024-10-08 09:01:48 -0500

    updated fuel calculations

--- index.js
+++ index.js
@@ -54,82 +54,82 @@
 // 28 MPG at 60 mph * 3 == $162 Below budget // 25 hrs
 // 23 MPG at 75 mph * 3 == $198 Over budget // 20 hrs
 
-const distance = 1500; // miles
-const speed_1 = 55; // 55 mph
-const speed_2 = 60; // 60 mph
-const speed_3 = 75; // 75 mph
+// const distance = 1500; // miles
+// const speed_1 = 55; // 55 mph
+// const speed_2 = 60; // 60 mph
+// const speed_3 = 75; // 75 mph
 
-const fuel_efficiency_55 = 30; // fuel efficiency of 30 miles/gallon at 55mph
-const fuel_efficiency_60 = 28; // fuel efficiency of 28 miles/gallon at 60 mph
-const fuel_efficiency_75 = 23; // fuel efficiency of 23 miles/gallon at 75 mph
+// const fuel_efficiency_55 = 30; // fuel efficiency of 30 miles/gallon at 55mph
+// const fuel_efficiency_60 = 28; // fuel efficiency of 28 miles/gallon at 60 mph
+// const fuel_efficiency_75 = 23; // fuel efficiency of 23 miles/gallon at 75 mph
 
-let budget = 175; // 175 dollars
-let avg_fuel_cost = 3; // 3 dollars/gallon
+// let budget = 175; // 175 dollars
+// let avg_fuel_cost = 3; // 3 dollars/gallon
 
-//How many gallons of fuel will you need for the entire trip?
-let total_gallons_55 = distance * (1 / fuel_efficiency_55);
-let total_gallons_60 = distance * (1 / fuel_efficiency_60);
-let total_gallons_75 = distance * (1 / fuel_efficiency_75);
-
-
-// Will your budget be enough for the trip
-let cost_55 = total_gallons_55 * avg_fuel_cost;
-let cost_60 = total_gallons_60 * avg_fuel_cost;
-let cost_75 = total_gallons_75 * avg_fuel_cost;
-
-let enough_55 = budget > cost_55;
-let enough_60 = budget > cost_60;
-let enough_75 = budget > cost_75;
-
-let results_55 = `At 55mph ${total_gallons_55} total gallons will be required for the trip. The cost will be ${cost_55}. The trip will take ${time_55} hours to complete.It is ${enough_55} that the budget is enough for the trip at 55mph`;
-
-let results_60 = `At 60mph ${total_gallons_60} total gallons will be required for the trip. The cost will be ${cost_60}. The trip will take ${time_60} hours to complete. It is ${enough_60} that the budget is enough for the trip at 60mph`;
-
-let results_75 = `At 75mph ${total_gallons_75} total gallons will be required for the trip. The cost will be ${cost_75}. The trip will take ${time_75} hours to complete.It is ${enough_75} that the budget is enough for the trip at 75mph`;
-
-console.log(results_55);
-console.log(results_60);
-console.log(results_75);
+// //How many gallons of fuel will you need for the entire trip?
+// let total_gallons_55 = distance * (1 / fuel_efficiency_55);
+// let total_gallons_60 = distance * (1 / fuel_efficiency_60);
+// let total_gallons_75 = distance * (1 / fuel_efficiency_75);
 
 
-const totalMileage = 1500;
-let speed1 = 55;
-let speed2 = 60;
-let speed3 = 75;
+// // Will your budget be enough for the trip
+// let cost_55 = total_gallons_55 * avg_fuel_cost;
+// let cost_60 = total_gallons_60 * avg_fuel_cost;
+// let cost_75 = total_gallons_75 * avg_fuel_cost;
 
-let efficiency1 = 30; //MPG
-let efficiency2 = 28; //MPG
-let efficiency3 = 23; //MPG
+// let enough_55 = budget > cost_55;
+// let enough_60 = budget > cost_60;
+// let enough_75 = budget > cost_75;
 
-let costOfFuel = 3; // dollars
-// let budget = 175; // dollars
+// let results_55 = `At 55mph ${total_gallons_55} total gallons will be required for the trip. The cost will be ${cost_55}. The trip will take ${time_55} hours to complete.It is ${enough_55} that the budget is enough for the trip at 55mph`;
+
+// let results_60 = `At 60mph ${total_gallons_60} total gallons will be required for the trip. The cost will be ${cost_60}. The trip will take ${time_60} hours to complete. It is ${enough_60} that the budget is enough for the trip at 60mph`;
+
+// let results_75 = `At 75mph ${total_gallons_75} total gallons will be required for the trip. The cost will be ${cost_75}. The trip will take ${time_75} hours to complete.It is ${enough_75} that the budget is enough for the trip at 75mph`;
+
+// console.log(results_55);
+// console.log(results_60);
+// console.log(results_75);
 
 
+// const totalMileage = 1500;
+// let speed1 = 55;
+// let speed2 = 60;
+// let speed3 = 75;
 
-let time_55 = totalMileage / speed1;
-let time_60 = totalMileage / speed1;
-let time_75 = totalMileage / speed1;
+// let efficiency1 = 30; //MPG
+// let efficiency2 = 28; //MPG
+// let efficiency3 = 23; //MPG
+
+// let costOfFuel = 3; // dollars
+// // let budget = 175; // dollars
 
 
 
-
-function calcGallonsNeeded(){
-  let gallonsNeeded = totalMileage/efficiency1;
-  console.log(`The trip will require ${gallonsNeeded} gallons at 55mph.`) 
-}
-function CalcMoneyNeed(){
-  let gallonsNeeded = totalMileage/efficiency1;
-  let moneyNeeded = gallonsNeeded * 3;
-  console.log(`The amount of money needed is $${moneyNeeded}.`)
-}
-
-function calcTimeNeeded(){
-  let timeNeeded = totalMileage / speed1
-  console.log(`The time it will take for this trip is ${timeNeeded} hours.`)
-}
+// let time_55 = totalMileage / speed1;
+// let time_60 = totalMileage / speed1;
+// let time_75 = totalMileage / speed1;
 
 
-calcGallonsNeeded();
-CalcMoneyNeed();
-calcTimeNeeded();
+
+
+// function calcGallonsNeeded(){
+//   let gallonsNeeded = totalMileage/efficiency1;
+//   console.log(`The trip will require ${gallonsNeeded} gallons at 55mph.`) 
+// }
+// function CalcMoneyNeed(){
+//   let gallonsNeeded = totalMileage/efficiency1;
+//   let moneyNeeded = gallonsNeeded * 3;
+//   console.log(`The amount of money needed is $${moneyNeeded}.`)
+// }
+
+// function calcTimeNeeded(){
+//   let timeNeeded = totalMileage / speed1
+//   console.log(`The time it will take for this trip is ${timeNeeded} hours.`)
+// }
+
+
+// calcGallonsNeeded();
+// CalcMoneyNeed();
+// calcTimeNeeded();
 
